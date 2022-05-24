0b797de8496cb6fe9737cf2d3ac57748b3629be3
Trung Anh <trantrunganh1505@gmail.com>
2022-05-24 08:52:39 +0700

    TTA add login-register

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from "./logo.svg"
-import "./App.css"
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<h1>Home page</h1>}></Route>
-                    <Route path="/login" element={<h1>Login page</h1>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
