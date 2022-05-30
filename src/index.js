e540f7a368dc4206e9e0247fcafc6a148570496a
Phi-Nguyen-git1520001 <nguyenbaphi2k1@gmail.com>
2022-05-30 08:01:53 +0700

    add cart

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
-console.log('abc');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/routers/router.js
+++ src/routers/router.js
@@ -2,6 +2,7 @@ import HomePage from "../pages/Home";
 import LoginPage from "../pages/Login";
 import ProfilePage from "../pages/Profile";
 import RegisterPage from "../pages/Register";
+import Cart from "../pages/Cart"
 
 const routers = [
   {
@@ -22,6 +23,11 @@ const routers = [
   {
     path: "/register",
     element: <RegisterPage />,
+    private: false,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
     private: false,
   },
 ];
